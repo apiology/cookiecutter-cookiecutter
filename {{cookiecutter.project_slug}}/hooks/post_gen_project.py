a79b5b39356d8c4ee5bd77d92e6d08908e950684
Vince Broz <vince@broz.cc>
2021-02-16 14:03:21 -0500

    Change post_gen_project.py to not run some hooks during upgrade

--- {{cookiecutter.project_slug}}/hooks/post_gen_project.py
+++ {{cookiecutter.project_slug}}/hooks/post_gen_project.py
@@ -16,23 +16,27 @@ if __name__ == '__main__':
 
     subprocess.check_call('./fix.sh')
     subprocess.check_call(['bundle', 'exec', 'overcommit', '--sign'])
-    subprocess.check_call(['git', 'init'])
-    subprocess.check_call(['git', 'add', '-A'])
-    subprocess.check_call(['git', 'commit', '-m',
-                           'Initial commit from boilerplate'])
-    if 'none' != '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}':
-        if 'private' == '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}':
-            visibility_flag = '--private'
-        elif 'public' == '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}':
-            visibility_flag = '--public'
-        else:
-            raise RuntimeError('Invalid argument to '
-                               'cookiecutter.type_of_github_repo: '
-                               '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}')
-        subprocess.check_call(['gh', 'repo', 'create',
-                               visibility_flag,
-                               '-y',
-                               '--description',
-                               '{% raw %}{{ cookiecutter.project_short_description }}{% endraw %}',
-                               '{% raw %}{{ cookiecutter.github_username }}/{% endraw %}'
-                               '{% raw %}{{ cookiecutter.project_slug }}{% endraw %}'])
+    if os.environ.get('IN_COOKIECUTTER_PROJECT_UPGRADER', '0') != '1':
+        # Don't run these non-idempotent things when in
+        # cookiecutter_project_upgrader, which will run this hook
+        # multiple times over its lifetime.
+        subprocess.check_call(['git', 'init'])
+        subprocess.check_call(['git', 'add', '-A'])
+        subprocess.check_call(['git', 'commit', '-m',
+                               'Initial commit from boilerplate'])
+        if 'none' != '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}':
+            if 'private' == '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}':
+                visibility_flag = '--private'
+            elif 'public' == '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}':
+                visibility_flag = '--public'
+            else:
+                raise RuntimeError('Invalid argument to '
+                                   'cookiecutter.type_of_github_repo: '
+                                   '{% raw %}{{ cookiecutter.type_of_github_repo }}{% endraw %}')
+            subprocess.check_call(['gh', 'repo', 'create',
+                                   visibility_flag,
+                                   '-y',
+                                   '--description',
+                                   '{% raw %}{{ cookiecutter.project_short_description }}{% endraw %}',
+                                   '{% raw %}{{ cookiecutter.github_username }}/{% endraw %}'
+                                   '{% raw %}{{ cookiecutter.project_slug }}{% endraw %}'])
