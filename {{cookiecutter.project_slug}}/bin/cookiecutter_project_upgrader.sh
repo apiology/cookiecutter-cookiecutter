d059a08cae0959b7f813ac1d21d8ae12fb85a76e
Test Test <vince@broz.cc>
2026-07-08 23:08:15 -0400

    Avoid Jinja {# in nested upgrader template during bake
    Use HIDDEN_BACKUP_COUNT instead of ${#HIDDEN_BACKUPS[@]} so cookiecutter
    does not treat {# as an unclosed comment in the nested template path.

--- {{cookiecutter.project_slug}}/bin/cookiecutter_project_upgrader.sh
+++ {{cookiecutter.project_slug}}/bin/cookiecutter_project_upgrader.sh
@@ -15,18 +15,20 @@ TEMPLATE_UPGRADE_BRANCH="${COOKIECUTTER_TEMPLATE_UPGRADE_BRANCH:-main}"
 GIT_WORKTREE_SHIM=0
 REPO_CWD="$(pwd)"
 HIDDEN_BACKUPS=()
+HIDDEN_BACKUP_COUNT=0
 
 hide_for_bake() {
   local src=$1 bak=$2
   if [ -f "${src}" ]; then
     mv "${src}" "${bak}"
     HIDDEN_BACKUPS+=("${bak}|${src}")
+    HIDDEN_BACKUP_COUNT=$((HIDDEN_BACKUP_COUNT + 1))
   fi
 }
 
 restore_hidden() {
   local entry bak dest
-  if [ ${#HIDDEN_BACKUPS[@]} -eq 0 ]; then
+  if [ "${HIDDEN_BACKUP_COUNT}" -eq 0 ]; then
     return 0
   fi
   for entry in "${HIDDEN_BACKUPS[@]}"; do
@@ -35,6 +37,7 @@ restore_hidden() {
     [ -f "${bak}" ] && mv "${bak}" "${dest}"
   done
   HIDDEN_BACKUPS=()
+  HIDDEN_BACKUP_COUNT=0
 }
 
 cleanup_prepare() {
